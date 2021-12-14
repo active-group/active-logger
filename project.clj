84fa74bd0fe88d2127684a4c241d882f5a462e3c
Marcus Crestani <marcus.crestani@active-group.de>
2021-12-14 14:55:00 +0100

    Bump and adjust dependencies.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/active-group/active-logger"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [;[org.clojure/clojure "1.9.0"]
-                 ;[org.slf4j/slf4j-log4j12 "1.7.9"] ;; breaks things when included in a project where log4j is already included from somewhere else?!
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [org.slf4j/slf4j-log4j12 "1.7.32"]
                  [com.taoensso/timbre "4.7.0"]
                  [de.active-group/active-clojure "0.35.0"]
                  [de.active-group/timbre-riemann "0.2.0"]
-                 [cheshire "5.8.0"] ;; for logstash 3rd-party-appender of timbre
-                 [riemann-clojure-client "0.5.1"]])
+                 [cheshire "5.10.1"] ;; for logstash 3rd-party-appender of timbre
+                 [riemann-clojure-client "0.5.1" :exclusions [org.slf4j/slf4j-api]]])
