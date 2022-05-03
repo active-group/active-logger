9fa7e383f44ab5796750cd0e2e19ce9817db5151
Marco Schneider <marco.schneider@active-group.de>
2022-05-03 16:11:57 +0200

    Remove org.sl4j/slf4j-log4j12, include slj4j deps.
    Basically follows the advice given here
    https://github.com/fzakaria/slf4j-timbre#other-logging-frameworks

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
-                 [org.slf4j/slf4j-log4j12 "1.7.32"]
+                 [com.fzakaria/slf4j-timbre "0.3.21"]
                  [com.taoensso/timbre "5.1.0"]
                  [de.active-group/active-clojure "0.40.0"]
                  [de.active-group/timbre-riemann "0.2.0"]
                  [cheshire "5.10.1"] ;; for logstash 3rd-party-appender of timbre
-                 [riemann-clojure-client "0.5.1" :exclusions [org.slf4j/slf4j-api]]])
+                 [riemann-clojure-client "0.5.1" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/log4j-over-slf4j "1.7.36"]
+                 [org.slf4j/jul-to-slf4j     "1.7.36"]
+                 [org.slf4j/jcl-over-slf4j   "1.7.36"]])
