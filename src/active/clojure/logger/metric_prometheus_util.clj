9468935cd763c55d2093469c0f37c9f9e2b19606
Marcus Crestani <marcus.crestani@active-group.de>
2025-07-16 09:04:51 +0200

    Add memoization to metric rendering.

--- src/active/clojure/logger/metric_prometheus_util.clj
+++ src/active/clojure/logger/metric_prometheus_util.clj
@@ -1,23 +1,32 @@
 (ns active.clojure.logger.metric-prometheus-util
   (:require [clojure.string :as string]))
 
-(defn cleanup-non-prometheus-label-characters
+(defn cleanup-non-prometheus-label-characters-1
   [s]
   (-> s
       (string/replace #"[^a-zA-Z0-9_]" "_")
       (string/replace #"(^[0-9])" "_\\1")))
 
-(defn render-label
+(def cleanup-non-prometheus-label-characters
+  (memoize cleanup-non-prometheus-label-characters-1))
+
+(defn render-label-1
   [k v]
   (str (cleanup-non-prometheus-label-characters (name k)) "=\"" v "\""))
 
-(defn render-labels
+(def render-label
+  (memoize render-label-1))
+
+(defn render-labels-1
   [labels]
   (if (empty? labels)
     ""
     (str "{"
          (string/join "," (mapv render-label (keys labels) (vals labels)))
          "}")))
+
+(def render-labels
+  (memoize render-labels-1))
 
 (defn render-value
   [v]
