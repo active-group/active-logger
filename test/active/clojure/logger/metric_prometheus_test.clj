a1170e19ee4ed8aa5aa4c214502df05e3e68f46c
David Frese <david.frese@active-group.de>
2026-07-15 14:12:15 +0200

    Add small benchmark for prometheus requests.

--- test/active/clojure/logger/metric_prometheus_test.clj
+++ test/active/clojure/logger/metric_prometheus_test.clj
@@ -1,6 +1,10 @@
 (ns active.clojure.logger.metric-prometheus-test
   (:require [active.clojure.logger.metric-prometheus :as m]
             [active.clojure.logger.metric-samples :as metric-samples]
+            [active.clojure.logger.metric-singular-value :as metric-singular-value]
+            [active.clojure.logger.metric-histogram-value :as metric-histogram-value]
+            [active.clojure.logger.metric-types :as metric-types]
+            [active.clojure.logger.metric-accumulator :as metric-accumulator]
             [clojure.test :as t]))
 
 (t/deftest t-render-metric-sets
@@ -47,3 +51,39 @@
                                      ;; MetricValue converts all values to double
                                      (double 8999999999999999991)
                                      0)])]))))
+
+(t/deftest benchmark-test
+  ;; plain mapping/string.join: 1061.141 msecs
+  (let [nmetrics 20000
+        nlabels 10
+        thresholds [5.0 10.0 50.0 80.0 99.0]
+        values [3892.0 230498.0 60000.0 38234.5 2339349.4]
+        ;; data is a map: {metric {labels histo-or-singular}}
+        data (into {}
+                   (map (fn [m]
+                          (let [t (case (mod m 3)
+                                    0 :counter
+                                    1 :gauge
+                                    2 :histogram)
+                                nbuckets (mod m 5)]
+                            [(case t
+                               :counter (metric-types/make-counter-metric (str m) "")
+                               :gauge (metric-types/make-gauge-metric (str m) "")
+                               :histogram (metric-types/make-histogram-metric (str m) ""
+                                                                              (take nbuckets thresholds)))
+                             (into {}
+                                   (map (fn [l]
+                                          ;; assuming the absolute values don't matter much for the rendering
+                                          [{:label (str l)
+                                            :info "host:999"}
+                                           (case t
+                                             :counter (metric-singular-value/make-metric-value 2.1 100)
+                                             :gauge (metric-singular-value/make-metric-value 982374.0 209384039)
+                                             :histogram (metric-histogram-value/make-histogram-metric-values 10000 4748823.0 4839 (take nbuckets values)))])
+                                        (range nlabels)))]))
+                        (range nmetrics)))]
+    (time
+     (let [s (-> data
+                 (metric-accumulator/all-snapshots->all-metric-sample-sets)
+                 (m/render-metric-sets))]
+       (t/is (string? s))))))
